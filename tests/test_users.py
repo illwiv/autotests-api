8d5de36357850068dd3a15b05ea4faec8fd66980
Roman <13rremezov@gmail.com>
2025-09-07 12:09:22 +0300

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,14 @@
+from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from http import HTTPStatus
 import pytest
 
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
 @pytest.mark.users
@@ -17,5 +20,17 @@ def test_create_user(public_users_client: PublicUsersClient):
 
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(response_data.user, function_user.response.user)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
+    GetUserResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,25 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет корректность данных пользователя
+    :param actual: Фактические данные
+    :param expected: Ожидаемые данные
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(get_user_response: UserSchema, create_user_response: UserSchema):
+    """
+    Проверяет, что данные пользователя при создании и при запросе совпадают
+    :param get_user_response: Данные при запросе пользователя
+    :param create_user_response: Данные при создании пользователя
+    """
+    assert_user(get_user_response, create_user_response)
